d1d4f15efa511e707d125484161475df9969d155
mindsky <romain.rondet@gmail.com>
2021-08-07 19:18:57 +0200

    add legal notice

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, legal_notice
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("legal_notice/", legal_notice, name="legal_notice"),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -8,3 +8,8 @@ from django.shortcuts import render
 def home(request):
     """Return home html template."""
     return render(request, "home/home.html")
+
+
+def legal_notice(request):
+    """Return home html template."""
+    return render(request, "home/legal_notice.html")
