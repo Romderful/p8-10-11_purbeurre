fa5c7ead1e4cbf0e8cee6b3c7a427cf2bdf4c38e
mindsky <romain.rondet@gmail.com>
2021-08-11 22:11:37 +0200

    update settings

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get("ENV", "development") == "production" else True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["rr-purbeurre.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
