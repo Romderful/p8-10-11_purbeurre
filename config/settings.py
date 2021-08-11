21a02fa0f9e71e5b20953ce58920599770a0d3d3
mindsky <romain.rondet@gmail.com>
2021-08-11 16:02:42 +0200

    prepare for deployment

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -25,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.environ.get("ENV", "development") == "production" else True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapps.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
@@ -143,3 +145,5 @@ AUTHENTICATION_BACKENDS = ["apps.users.authenticate.EmailAuthenticate"]
 LOGIN_URL = "/users/sign_in"
 
 AUTH_USER_MODEL = "users.User"
+
+django_heroku.settings(locals())
